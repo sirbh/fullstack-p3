5fca10739f7b89fab47ba91401497e74c057a036
saurabh chauhan <sourabh1202@gmail.com>
2023-02-08 22:53:54 +0200

    fixed middleware ordering

--- index.js
+++ index.js
@@ -33,17 +33,15 @@ morgan.token("content", (req, res) => {
   return "";
 });
 
-app.use(express.static('build'))
 
 app.use(cors());
-
+app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :content"
   )
 );
-
-app.use(express.json());
+app.use(express.static('build'))
 
 app.get("/info", (request, response) => {
   response.send(
